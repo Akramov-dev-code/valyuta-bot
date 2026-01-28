af50560e84fb0041c36a9f905042daa218bd3540
Abdulloh-dev-code <abdullohmirzaryev1998@gmail.com>
2026-01-28 10:27:41 +0500

    narx hisoblash funksiyasi qo'shildi

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from telebot import TeleBot
 from config import token
 from telebot.types import Message
 from utils import get_all_currency
-
+from telebot.util import extract_arguments
 
 bot = TeleBot(token)
 
+currencies = get_all_currency()
 
 
 #  start buyrug'i
@@ -17,7 +18,6 @@ def start_handler(message: Message):
 
 @bot.message_handler(commands=["all"])
 def all_currency_handler(message: Message):
-    currencies = get_all_currency()
     if not currencies:
         bot.send_message(message.chat.id, "Menda ma'lumotlar topilmadi🥴")
         return
@@ -30,7 +30,21 @@ def all_currency_handler(message: Message):
 
 @bot.message_handler(commands=["usd"])
 def usd_handler(message: Message):
-    pass
+    price = int(currencies[0][1])
+    args = extract_arguments(message.text)
+    
+    if args and not args.isdigit():
+        bot.send_message(message.chat.id, "Noto'g'ri buyruq!\nMa'lumot uchun /info")
+        return
+    
+    
+    count = 1
+    if args:
+       count = int(args)
+    
+    bot.send_message(message.chat.id, f"{count} 💲 - {count * price} so'm") 
+    
+        
 
 @bot.message_handler(commands=["eur"])
 def euro(message: Message):
